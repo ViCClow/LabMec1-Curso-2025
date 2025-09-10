c7b512fe7f47b302deacf5fbd212ecb9ca301428
Victor Garcia <vgrcia.orellana022@gmail.com>
2025-09-10 16:31:45 -0300

    Se agrego carpeta src. Trabajar ahí.

---
+++ src/CaidaLibre_LM1.cpp
@@ -0,0 +1,150 @@
+/* ===== EXPERIMENTO CAIDA LIBRE ===== */
+// Codigo utilizado en el experimento de caida libre diseñado para
+// el curso Laboratorio 1: Mecanica I del segundo (2°) semestre de la
+// carrera de Ciencias Fisicas - FCFM - Universidad de Concepcion.
+
+/* Librerias */
+
+//#include "Adafruit_VL53L0X.h" //Libreria para sensor ToF
+
+#include <Arduino.h>
+
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+/* ============================= */
+
+void timeDisplay(int a, int b, double c) ;
+void TemplateDisplay();
+
+const int BAUDRATE = 9600;
+
+LiquidCrystal_I2C lcd(0x27, 20, 4);
+
+double t_inicial = 0;
+double t1, t2, t3, t4 = 0;
+bool start = 1;
+
+
+const int laserPins[] = { 3, 5};           // Pines conectados a los láseres
+const int photoResPins[] = { 4, 6};   // Pines conectados a las fotoresistencias
+int button = 2;                       // Pin de reset de la barrera
+
+int numPairs = sizeof(laserPins)/sizeof(laserPins[0]);  // Numero de modulos
+
+bool photoResStates[sizeof(photoResPins)/sizeof(photoResPins[0])]; // Estados actuales
+bool photoResPrevStates[sizeof(photoResPins)/sizeof(photoResPins[0])]; // Estados previos
+
+
+void setup() {
+  Serial.begin(BAUDRATE);
+
+  lcd.init();
+  lcd.backlight();
+
+  t_inicial = millis();
+
+  pinMode(button, INPUT_PULLUP);
+
+  // Initialize pins
+  for (int i = 0; i < numPairs; ++i) {
+    pinMode(laserPins[i], OUTPUT);
+    digitalWrite(laserPins[i], HIGH);
+
+    pinMode(photoResPins[i], INPUT_PULLUP);
+
+    photoResPrevStates[i] = digitalRead(photoResPins[i]);
+  }
+}
+
+void loop() {
+
+  TemplateDisplay();
+
+  //============ Restart button ============//
+  //  For debugging uncomment the Serial.println line
+  bool pressed = digitalRead(button); // Standby state = 0
+  //Serial.println(pressed); 
+
+  //=========== Read photoresistors ============//
+  // For debugging uncomment the Serial.print lines:
+  for (int i = 0; i < numPairs; ++i) {
+    photoResStates[i] = digitalRead(photoResPins[i]);
+    //Serial.print("Sensor "); Serial.print(i); Serial.print(": "); Serial.println(photoResStates[i]);
+  }
+
+  //=========== Reset ============//
+  if (pressed) {
+    t1 = 0;
+    t2 = 0;
+    t3 = 0;
+    t4 = 0;
+
+    t_inicial = (double)millis();
+    for (int i = 0; i < numPairs; ++i) {
+      digitalWrite(laserPins[i], HIGH);
+      photoResPrevStates[i] = photoResStates[i]; //Reinicia estado previo.
+    }
+  
+    lcd.clear();
+    TemplateDisplay();
+    start = 1;
+  }
+
+  //=========== Measure time ============//
+    // Para B1
+    if (photoResPrevStates[0] == LOW && photoResStates[0] == HIGH && t1 == 0) {
+      t1 = ((double)millis() - t_inicial) / 1000.0;  // Tiempo transcurrido tras pasar primera barrera
+      digitalWrite(laserPins[0], LOW);
+      timeDisplay(0,3,t1);
+    }
+    // Para B2
+    if (photoResPrevStates[1] == LOW && photoResStates[1] == HIGH && t2 == 0) {
+      t2 = ((double)millis() - t_inicial) / 1000.0;  // Tiempo transcurrido tras pasar segunda barrera
+      digitalWrite(laserPins[1], LOW);
+      timeDisplay(1,3,t2);
+    }
+  
+  // ========== Actualizar estados previos. ========== //
+  for (int i = 0; i < numPairs; ++i) {
+    photoResPrevStates[i] = photoResStates[i];
+  }
+
+    //=========== Desactivar barrera. ===========//
+
+}
+
+//============ Funciones (Try objects when you can.) ===========//
+
+void timeDisplay(int a, int b, double c) {
+  lcd.setCursor(a, b);
+  lcd.print(c);
+  lcd.print("s");
+
+return ;
+}
+
+void TemplateDisplay() {
+
+  lcd.setCursor(0, 0);
+  lcd.print("B1:");
+  lcd.print(t1);
+  lcd.print("s");
+
+  lcd.setCursor(0, 1);
+  lcd.print("B2:");
+  lcd.print(t2);
+  lcd.print("s");
+
+//  lcd.setCursor(0, 2);
+//  lcd.print("B3:");
+//  lcd.print(t3);
+//  lcd.print("s");
+//
+  //lcd.setCursor(0, 3);
+  //lcd.print("B4:");
+  //lcd.print(t4);
+  //lcd.print("s");
+
+return ;
+}
